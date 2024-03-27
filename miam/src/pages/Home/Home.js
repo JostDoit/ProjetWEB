26dd93e1bf5d6d1752e0f471b682b1aecf03b4ca
EnguerranC <enguerran.cheneau@gmail.com>
2024-03-27 15:44:29 +0100

    premiere ebauche du site

---
+++ miam/src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="home">
+            <h1>Bienvenue sur notre site</h1>
+            {/* Autres éléments de la page d'accueil */}
+        </div>
+    );
+}
+
+export default Home;
